96d62391369eee476855ee47fafcf1836b84cc3e
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-22 08:59:50 +0530

    1st use case

--- src/working/EmployeePresent.java
+++ src/working/EmployeePresent.java
@@ -4,7 +4,16 @@ public class EmployeePresent {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int IS_FULL_TIME=1;
+		double empCheck=(Math.floor(Math.random()*10)%2);
+		if(IS_FULL_TIME==empCheck)
+		{
+			System.out.println("Full time");
+		}
+		else
+		{
+			System.out.println("Not full time");
+		}
 	}
 
 }
